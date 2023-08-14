16739fdf130daa0ae7a726133e4667ab8264c6d1
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-08-14 14:25:14 +0100

    feito

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,4 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
-
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+require "test_helper"
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :headless_chrome # Update this line
 end
